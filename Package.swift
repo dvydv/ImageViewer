a28dd89c7e6b45a3e29cb885e2087b9e63f12afd
Anton Davydov <ant.davydov@icloud.com>
2022-06-20 15:38:10 +0400

    fixed SPM

--- Package.swift
+++ Package.swift
@@ -1,15 +1,19 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.1
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "ImageViewer",
+    platforms: [
+        .iOS(.v12)
+    ],
     products: [
         .library(
             name: "ImageViewer",
             targets: ["ImageViewer"]),
     ],
+    dependencies: [],
     targets: [
         .target(
             name: "ImageViewer",
